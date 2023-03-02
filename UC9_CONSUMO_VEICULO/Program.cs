3f3f71e67aeb9b131b97d89bbe0c2cc1acb4d2e7
LUCAS GOMES GALVAO <lucas.ggalvao@senacsp.edu.br>
2023-03-01 21:55:41 -0300

    veiuculo

---
+++ UC9_CONSUMO_VEICULO/Program.cs
@@ -0,0 +1,19 @@
+﻿double valorabastecido, kmrodado, valorlitro, consumo;
+
+valorabastecido = 0;
+kmrodado = 0;
+valorlitro = 0;
+consumo = 0;
+
+Console.WriteLine("Qual o valor pago pelo abastecimento?");
+valorabastecido = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Quantos KMs você percorreu com este abastecimento?");
+kmrodado = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Qual o valor do Litro de Combustível?");
+valorlitro = double.Parse(Console.ReadLine());
+
+consumo = kmrodado / (valorabastecido / valorlitro);
+
+Console.WriteLine("O consumo do seu veículo é (KM / l): " + consumo);
